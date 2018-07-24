2a1921545d26afae4f7e69ee8423b89121535573
Murugesan <RAJASHUNMUGAM_MURUGESAN@homedepot.com>
2018-07-24 10:08:57 -0500

    Initial commit

---
+++ src/main/java/com/homedepot/mm/KafkaMessageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.homedepot.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageServiceApplication.class, args);
+	}
+}
